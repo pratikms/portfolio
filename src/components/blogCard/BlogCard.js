2254f910d88346fd2068ece7a5a363407928ff41
Pratik Shivaraikar <pratik.shivaraikar@gmail.com>
2020-02-28 17:49:31 +0530

    Update blog card

--- src/components/blogCard/BlogCard.js
+++ src/components/blogCard/BlogCard.js
@@ -2,18 +2,24 @@ import React from "react";
 import "./BlogCard.css";
 
 export default function BlogCard({ blog }) {
+
+  function openBlogNewTab(url) {
+    var win = window.open(url, "_blank");
+    win.focus();
+  }
+
   return (
     <div>
-      <div class="container">
-        <div class="square">
+      <div className="container">
+        <div className="square" onClick={() => openBlogNewTab(blog.url)}>
           <img src={blog.image} alt="blog" className="blog-card-image mask" />
-          <div class="blog-card-title">{blog.title}</div>
+          <div className="blog-card-title">{blog.title}</div>
           <p className="blog-card-subtitle">{blog.description}</p>
 
           <div>
-            <a href={blog.url} target="_" class="button">
+            {/* <a href={blog.url} target="_" class="button">
               Read More
-            </a>
+            </a> */}
           </div>
         </div>
       </div>
