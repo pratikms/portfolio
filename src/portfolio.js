2a4273027c7a2ee66c5dbcc75853db9934bdc2e4
Pratik Shivaraikar <pratik.shivaraikar@gmail.com>
2020-02-28 23:00:00 +0530

    Revert "Updates"
    This reverts commit 516dc2fd12ac7a74479d0b848125fe814919419b.

---
+++ src/portfolio.js
@@ -0,0 +1,195 @@
+/* Change this file to get your personal Porfolio */
+
+const greeting = {
+  /* Your Summary And Greeting Section */
+  title: "Hello, World! 👋 I'm Pratik Shivaraikar",
+  subTitle:
+    "I'm a passionate software engineer based in Pune, India specializing in building exceptional, high-quality software products.",
+  resumeLink: "https://drive.google.com/file/d/1IJCQ86t7j7hDFQS7j-mQrZ_WpsscS4HU/view?usp=sharing"
+};
+
+const socialMediaLinks = {
+  /* Your Social Media Link */
+  github: "https://github.com/pratikms",
+  linkedin: "https://www.linkedin.com/in/pratik-shivaraikar/",
+  gmail: "pratik.shivaraikar@gmail.com",
+  // gitlab: "https://gitlab.com/pratikms",
+  // facebook: "https://www.facebook.com/pratik.shivaraikar"
+};
+
+const skillsSection = {
+  /* Your Skills Section  */
+  title: "About Me",
+  subTitle: "I have a professional experience of working in the Storage, Security and Wireless domains. Apart from that, I've also worked on quite a few open-source projects ranging from web development to game development.",
+  skillsPreamble: "Here are a few technologies that I've worked on:",
+  // skills: [
+  //   "⚡ Develop highly interactive Front end / User Interfaces for your web and mobile applications",
+  //   "⚡ Progressive Web Applications ( PWA ) in normal and SPA Stacks",
+  //   "⚡ Integration of third party services such as Firebase/ AWS / Digital Ocean"
+  // ],
+  
+  /* Make Sure You include correct Font Awesome Classname to view your icon */
+  /* https://fontawesome.com/icons?d=gallery */
+  
+  softwareSkills: [
+    {
+      skillName: "python",
+      fontAwesomeClassname: "fab fa-python"
+    },
+    {
+      skillName: "nodejs",
+      fontAwesomeClassname: "fab fa-node"
+    },
+    {
+      skillName: "PHP",
+      fontAwesomeClassname: "fab fa-php"
+    },
+    {
+      skillName: "JavaScript",
+      fontAwesomeClassname: "fab fa-js"
+    },
+    {
+      skillName: "reactjs",
+      fontAwesomeClassname: "fab fa-react"
+    },
+    {
+      skillName: "aws",
+      fontAwesomeClassname: "fab fa-aws"
+    },
+    {
+      skillName: "firebase",
+      fontAwesomeClassname: "fas fa-fire"
+    },
+    {
+      skillName: "docker",
+      fontAwesomeClassname: "fab fa-docker"
+    },
+    {
+      skillName: "java",
+      fontAwesomeClassname: "fab fa-java"
+    },
+    {
+      skillName: "sql-database",
+      fontAwesomeClassname: "fas fa-database"
+    },
+  ]
+};
+
+const openSource = {
+  /* Your Open Source Section to View Your Github Pinned Projects */
+  /* To know how to get github key look at readme.md */
+
+  githubConvertedToken: process.env.REACT_APP_GITHUB_ACCESS_TOKEN,
+  githubUserName: "pratikms"
+};
+
+const bigProjects = {
+  /* Some Big Projects You have worked with your company */
+  title: "Big Projects",
+  subtitle: "SOME STARTUPS AND COMPANIES THAT I HELP TO CREATE THEIR TECH",
+  projects: [
+    {
+      image: "https://drive.google.com/uc?id=1exWn9T6j8TsfDDHJnS3VR66eP6RiGAfY",
+      link: "http://saayahealth.com/"
+    },
+    {
+      image: "http://nextu.se/wp-content/themes/nextu/images/og_img.jpg",
+      link: "http://nextu.se/"
+    }
+  ]
+};
+
+const achievementSection = {
+  /* Your Achievement Section Include Your Certification Talks and More */
+
+  title: "Achievements And Certifications 🏆 ",
+  subtitle: "Achievements, Certifications Award Letters and Some Cool Stuff that i have done !",
+
+  achivementsCards: [
+    {
+      title: "Google Code-In Finalist",
+      description: "First Pakistani to be selected as Google Google-in Finalist from 4000 students from 77 diffrent countries",
+      image:
+        "https://1.bp.blogspot.com/-Ig-v1tDXZt4/XDODmZvWp1I/AAAAAAAAB0A/KtbFdBPFVQw2O15FekkIR0Yg8MUp--rngCLcBGAs/s1600/GCI%2B-%2BVertical%2B-%2BGray%2BText%2B-%2BWhite%2BBG.png",
+      footerLink: [
+        { name: "Certification", url: "https://drive.google.com/file/d/0B7kazrtMwm5dYkVvNjdNWjNybWJrbndFSHpNY2NFV1p4YmU0/view?usp=sharing" },
+        { name: "Award Letter", url: "https://drive.google.com/file/d/0B7kazrtMwm5dekxBTW5hQkg2WXUyR3QzQmR0VERiLXlGRVdF/view?usp=sharing" },
+        { name: "Google Code-in Blog", url: "https://opensource.googleblog.com/2019/01/google-code-in-2018-winners.html" }
+      ]
+    },
+    {
+      title: "Google Assistant Action",
+      description: "Developed a Google Assistant Action JavaScript Guru that is available on 2 Billion devices world wide.",
+      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cb/Google_Assistant_logo.svg/1200px-Google_Assistant_logo.svg.png",
+      footerLink: [{ name: "View Google Assistant Action", url: "https://assistant.google.com/services/a/uid/000000100ee688ee?hl=en" }]
+    },
+
+    {
+      title: "PWA Web App Developer",
+      description: "Completed Certifcation from SMIT for PWA Web App Development",
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRu93Bd6LDbxPEOXr-hfLqLYzmHny8c0MJoI3exQP-lwpFLRT7g&s",
+      footerLink: [
+        { name: "Certification", url: "" },
+        { name: "Final Project", url: "https://pakistan-olx-1.firebaseapp.com/" }
+      ]
+    }
+  ]
+};
+
+const blogSection = {
+  /* Blogs Section */
+
+  title: "Blogs",
+  subtitle: "Whenever I find time, I also like to write about whatever I have learned:",
+
+  blogs: [
+    {
+      url: "https://pratikms.hashnode.dev/securing-cloud-infrastructure-with-ids-ck34hflpo0089pcs1ystafokn",
+      image: "https://cdn.hashnode.com/res/hashnode/image/upload/v1582556518552/zC3mA8OOx.jpeg",
+      title: "Securing Cloud with IDS",
+      description: "An insight into securing your cloud infrastructure using open source technologies."
+    },
+    {
+      url: "https://pratikms.hashnode.dev/the-django-jargon-ck1zi1844011t9ns1x1yof2hv",
+      image: "https://cdn.hashnode.com/res/hashnode/image/upload/v1571606086498/3mJWKc1Ss.png",
+      title: "The Django Jargon",
+      description: "Discussion about the typical jargons one encounters while dealing with Django for the first time."
+    }
+  ]
+};
+
+// Talks Sections
+
+const talkSection = {
+  // title: "TALKS",
+  // subtitle: "I LOVE TO SHARE MY LIMITED KNOWLEDGE AND GET A SPEAKER BADGE 😅",
+
+  // talks: [
+  //   {
+  //     title: "Build Actions For Google Assistant",
+  //     subtitle: "Codelab at GDG DevFest Karachi 2019",
+  //     slides_url: "https://bit.ly/saadpasta-slides",
+  //     event_url: "https://www.facebook.com/events/2339906106275053/",
+  //     image:
+  //       "https://scontent.fkhi6-1.fna.fbcdn.net/v/t1.0-9/76714032_1730516240415559_1293494289556307968_o.jpg?_nc_cat=103&_nc_ohc=s5f81rdZd6wAQmqUa52YQBA21MjVOy7e-HN9jI7MspRDC-v5ahEouyInQ&_nc_ht=scontent.fkhi6-1.fna&oh=c29141a9a0de7ff2011a8191f5475a78&oe=5E83127C"
+  //   }
+  // ]
+};
+
+// Podcast Section
+const podcastSection = {
+  // title: "Podcast 🎙️",
+  // subtitle: "I LOVE TO TALK ABOUT MYSELF AND TECHNOLOGY",
+
+  // // Please Provide with Your Podcast embeded Link
+  // podcast: ["https://anchor.fm/codevcast/embed/episodes/DevStory---Saad-Pasta-from-Karachi--Pakistan-e9givv/a-a15itvo"]
+};
+
+const contactInfo = {
+  title: "Get In Touch",
+  subtitle: "Although I'm not currently looking for freelance opportunities, my inbox is always open. Whether for a potential project or just to say hi, I'll try my best to answer your email!",
+  // number: "+92-3243454077",
+  email_address: "pratik.shivaraikar@gmail.com"
+};
+
+export { greeting, socialMediaLinks, skillsSection, openSource, bigProjects, achievementSection, blogSection, talkSection, podcastSection, contactInfo };
