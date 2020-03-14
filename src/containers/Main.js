01ebbe899e0b369af445864452a195d329c17f81
Pratik Shivaraikar <pratik.shivaraikar@gmail.com>
2020-03-14 13:10:32 +0530

    Add Main section

---
+++ src/containers/Main.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react"
+
+import Header from "../components/header/Header"
+import Greeting from "./greeting/Greeting"
+// import Skills from "./skills/Skills";
+// import Projects from "./projects/Projects";
+// import Blogs from "./blogs/Blogs";
+// import Contact from "./contact/Contact";
+import Footer from "../components/footer/Footer"
+import "./Main.css"
+
+export default class Main extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Greeting />
+        {/* <Skills />
+        <Projects />
+        <Blogs />
+        <Contact /> */}
+        {/* <h1>Hello, world!</h1> */}
+        <Footer />
+      </div>
+    );
+  }
+}
