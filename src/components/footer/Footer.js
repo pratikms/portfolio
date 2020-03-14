7807286a75d9173fffce3aaf279e4396b60e0aca
Pratik Shivaraikar <pratik.shivaraikar@gmail.com>
2020-03-14 13:45:29 +0530

    Fix CSS conflicts

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -5,6 +5,7 @@ export default function Footer() {
   return (
     <div className="footer-div">
       <p className="footer-text">Made with <span role="img" aria-label="love">❤️</span> by Pratik Shivaraikar</p>
+      <p className="footer-text">© {new Date().getFullYear()}</p>
     </div>
   );
 }
