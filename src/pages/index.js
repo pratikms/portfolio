08cdd31949b908dd7639e843b2260b6ed775fc3f
Pratik Shivaraikar <pratik.shivaraikar@gmail.com>
2020-03-14 21:32:19 +0530

    Add SEO

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import React from "react"
 
 // import Layout from "../components/layout"
 // import Image from "../components/image"
-import SEO from "../components/seo"
+import SEO from "../components/seo/Seo"
 import Main from "../containers/Main"
 import '../styles/index.css'
 import '../styles/font-awesome/css/all.css'
@@ -11,7 +11,10 @@ import '../styles/font-awesome/css/all.css'
 
 const IndexPage = () => (
   <>
-    <SEO title="Pratik Shivaraikar" />
+    <SEO 
+      title="Pratik Shivaraikar"
+      desc="Software Engineer. Pratik Shivaraikar's personal website." 
+    />
     <Main />
   </>
 )
